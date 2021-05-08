f8c38ad188374e42a86aac29f472af4aaf519915
lebcel <64216836+lebcel@users.noreply.github.com>
2021-05-08 14:56:05 +0200

    Add files via upload
    Seule la somme ne fonctionne pas mais tout le reste devrait fonctionner.

--- FichiersTIF.py
+++ FichiersTIF.py
@@ -27,7 +27,7 @@ from math import sqrt
 ###########################################################################
 # Import des fichiers : Crop Mask + Hist Yield
 
-monRep=r'C:\Users\lebce\OneDrive\Documents\DataForGood\0 - Data\Data for Good (saison 9) - projet GeoWatch Labs\GeoWatch Labs Agricultural Maps\Crop masks'
+monRep=r'C:\0 - Data\GeoWatch Labs Agricultural Maps\Crop masks'
 liCropMask = []
 liCropMask_np = []
 for path, subdirs, files in os.walk(monRep):
@@ -48,8 +48,7 @@ for path, subdirs, files in os.walk(monRep):
             liCropMask_np.append([rast_rend.read(1)])
 
 
-
-monRep=r'C:\Users\lebce\OneDrive\Documents\DataForGood\0 - Data\Data for Good (saison 9) - projet GeoWatch Labs\GeoWatch Labs Agricultural Maps\Historical Yields'
+monRep=r'C:\0 - Data\GeoWatch Labs Agricultural Maps\Historical Yields'
 liHistYield = []
 liHistYield_np = []
 for path, subdirs, files in os.walk(monRep):
@@ -70,6 +69,7 @@ for path, subdirs, files in os.walk(monRep):
             zone=name[0:len(name)-len(temp)-1]
             liHistYield.append([annee,prod,path,name,zone,rast_rend,rast_rend.height,rast_rend.width])
             liHistYield_np.append([rast_rend.read(1)])
+
 
 # nettoyage
 del monRep ,rast_rend,path, subdirs, files,fileExtension, name, annee, temp,prod,zone
@@ -98,17 +98,43 @@ del List_drop
 
 
 
-
 ###########################################################################
 # chargement de la base des ménages
 
-df_analyse=fs.df_analyse.copy()
-#df_analyse=fs.df_analyse.loc[df_analyse.NUMQUEST==203108.0,:]
+liste=[203108.0,31113.0,100101.0,100114.0,100103.0,203613.0,100115.0,203614.0,
+      203601.0,10610.0,100113.0,203605.0,31103.0,31105.0,203612.0,100102.0,31008.0,
+      203606.0,203611.0,10604.0,203615.0,31104.0,203602.0,31101.0,31109.0,203607.0,
+      31107.0,31111.0,203608.0,203603.0,31106.0,31110.0]
+
+df_analyse=fs.df_analyse.loc[fs.df_analyse.NUMQUEST.isin(liste),:]
+
+'''
+df_analyse["LATITUDE2"]=pd.to_numeric(df_analyse["LATITUDE"], errors='coerce')
+
+a=df_analyse["LATITUDE"].astype('int64')
+a=float(df_analyse["LATITUDE"])
+
+
+DataFrame.astype
+Cast argument to a specified dtype.
+
+to_datetime
+Convert argument to datetime.
+
+to_timedelta
+Convert argument to timedelta.
+
+numpy.ndarray.astype
+Cast a numpy array to a specified type.
+
+DataFrame.convert_dtypes
+Convert dtypes.
+'''
 
 ###########################################################################
 # calcul des coordonnées sur toute la base Rendements
 
-# calcul des rendements par annee, prod et zone pour chaque ménage
+# calcul des rendements par annee, prod et zone pour chaque ménage 
 for i in range(len(liHistYield)) :
     temp=liHistYield[i][0]+"_"+liHistYield[i][1]+"_"+liHistYield[i][4] # annee + prod + zone
     df_analyse["HY_row_"+temp],df_analyse["HY_col_"+temp] = liHistYield[0][5].index(pd.to_numeric(df_analyse["LONGITUDE"], errors='coerce'), 
@@ -123,9 +149,12 @@ for i in range(len(liHistYield)) :
     
     #df_analyse["HY_Rend_"+temp]=liHistYield_np[i][0][df_analyse["HY_row_"+temp]][df_analyse["HY_col_"+temp]]
     coords_list = [(df_analyse["HY_row_"+temp][k], df_analyse["HY_col_"+temp][k]) for k in df_analyse.index]
-    df_analyse["HY_Rend_"+temp]= pd.DataFrame( [liHistYield_np[i][0][coords] for coords in coords_list],columns=["HY_Rend_"+temp])
+    rent_list = [liHistYield_np[i][0][coords] for coords in coords_list]
+    df_analyse.loc[:,"HY_Rend_"+temp] = rent_list
 
-del max_row, max_col, temp, i, coords_list
+#liHistYield_np[6][0][(310, 1700)]
+
+del max_row, max_col, temp, i, coords_list, rent_list
 
 # somme des rendements par annee, prod pour chaque ménage
 for an in range(len(list_annee_HY)) :
@@ -139,7 +168,7 @@ del an, prod, nom, list
 # affichage des résultats
 list = [col for col in df_analyse.columns if col.startswith('HY_Rend_som_')]
 a=df_analyse[list]
-
+df_analyse["es"]=""
 del a, list
 
 # somme des rendements par prod pour chaque ménage en fonction de la date
@@ -179,13 +208,12 @@ for i in range(len(liCropMask)) :
     
     df_analyse.loc[df_analyse["CM_col_"+temp]< 0 , ["CM_col_"+temp]]=0
     df_analyse.loc[df_analyse["CM_col_"+temp]> max_col , ["CM_col_"+temp]]=0
-    
-    #df_analyse["CM_Rend_"+temp]=liCropMask_np[i][0][df_analyse["CM_row_"+temp]][df_analyse["CM_col_"+temp]]
-    #df_analyse["CM_Rend_"+temp]=liCropMask_np[i][0][309][502]
+
     coords_list = [(df_analyse["CM_row_"+temp][k], df_analyse["CM_col_"+temp][k]) for k in df_analyse.index]
-    df_analyse["CM_Rend_"+temp]= pd.DataFrame( [liCropMask_np[i][0][coords] for coords in coords_list],columns=["CM_Rend_"+temp])
-   
-del max_row, max_col, temp, i, coords_list
+    rent_list = [liCropMask_np[i][0][coords] for coords in coords_list]
+    df_analyse.loc[:,"CM_Rend_"+temp] = rent_list
+
+del max_row, max_col, temp, i, coords_list, rent_list
 
 # somme des rendements par annee, prod pour chaque ménage
 for an in range(len(list_annee_CM)) :
@@ -198,6 +226,9 @@ del an, prod, nom, list
 
 # affichage des résultats
 list = [col for col in df_analyse.columns if col.startswith('CM_Rend_som_')]
+a=df_analyse[list]
+
+list = [col for col in df_analyse.columns if ('sorghum') in col]
 a=df_analyse[list]
 
 del a, list
@@ -225,8 +256,6 @@ a=df_analyse[list]
 del a, list
 
 ###########################################################################
-
-
 
 
 
